87d54bc18e3f34d1cb9b64b370c10d2415a26f55
bradymat <bradymat@gmail.com>
2016-12-05 15:45:10 +1300

    basic vr working

--- components/index.js
+++ components/index.js
@@ -1,14 +1,15 @@
 import html from 'yo-yo'
-import header from './header'
-import link from './buttons/link'
+import 'aframe'
+// import header from './header'
+// import link from './buttons/link'
 
 module.exports = (state, dispatch) => html`
     <div>
-      ${header(state, dispatch)}
-      <p>
-        A scalable client side routing boilerplate made by
-        ${link('bradymat', 'https://github.com/bradymat')}
-        for the fastest web experience
-      </p>
-    </div>
+      <a-scene>
+        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+        <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+        <a-sky color="#ECECEC" src="https://rawgit.com/aframevr/assets/gh-pages/360-image-gallery-boilerplate/img/sechelt.jpg"></a-sky>
+    </a-scene>
+  </div>
   `
+  // <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
