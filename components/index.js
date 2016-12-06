bb0630d00e111aa538328bf2f791ec4e0d1aae5f
bradymat <bradymat@gmail.com>
2016-12-06 13:54:20 +1300

    hello piwakawaka

--- components/index.js
+++ components/index.js
@@ -1,15 +1,11 @@
 import html from 'yo-yo'
-import 'aframe'
-// import header from './header'
-// import link from './buttons/link'
+import header from './header'
+import link from './buttons/link'
+import button from './buttons/custom'
 
 module.exports = (state, dispatch) => html`
-    <div>
-      <a-scene>
-        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-        <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-        <a-sky color="#ECECEC" src="https://rawgit.com/aframevr/assets/gh-pages/360-image-gallery-boilerplate/img/sechelt.jpg"></a-sky>
-    </a-scene>
+  <div>
+    ${header(state)}
+    ${button('enter', dispatch, 'CHANGE_ROUTE', '/info')}
   </div>
   `
-  // <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
--- components/info.js
+++ components/info.js
@@ -1,16 +1,34 @@
 import html from 'yo-yo'
-import header from './header'
-import button from './buttons/custom'
-import link from './buttons/link'
+import 'aframe'
+import 'aframe-text-component'
+import extras from 'aframe-extras'
+
+AFRAME.registerComponent('checkpoint', extras.misc.checkpoint)
+extras.controls.registerAll()
+extras.registerAll()
 
 module.exports = (state, dispatch) => html`
     <div>
-      ${header(state, dispatch)}
-      ${button('back', dispatch, 'CHANGE_ROUTE', '/')}
-      <h3>Technologies used</h3>
-      <p>${link('yo-yo', 'https://www.npmjs.com/package/yo-yo')}</p>
-      <p>${link('sheet-router', 'https://www.npmjs.com/package/sheet-router')}</p>
-      <p>${link('redux', 'https://www.npmjs.com/package/redux')}</p>
-      ${button('lemon', dispatch, 'CHANGE_ROUTE', '/lemon')}
+    <a-scene>
+      <a-assets>
+        <a-asset-item id="knight" src="/components/assets/knight.ply"></a-asset-item>
+        <a-asset-item id="old" src="/components/assets/old.ply"></a-asset-item>
+        <a-asset-item id="cloud1" src="/components/assets/cloud1.ply"></a-asset-item>
+        <a-asset-item id="castle" src="/components/assets/castle.ply"></a-asset-item>
+      </a-assets>
+
+      <a-cylinder position="0 -40 0" radius="1000" height="10" color="#7CFC00"></a-cylinder>
+      <a-sphere position="30 150 -120" radius="20" color="#FFC65D"></a-sphere>
+      <a-entity position="-50 20 -80" text="text: Hello; size: 20;" material="color: #FFF" ></a-entity>
+      <a-entity position="-70 0 -80" text="text: Piwakawaka; size: 20;" material="color: #FFF" ></a-entity>
+      <a-entity position="20 -30 -100" ply-model="src: #knight" rotation="-90 0 0"></a-entity>
+      <a-entity position="0 -30 -100" ply-model="src: #old" rotation="-90 0 0"></a-entity>
+      <a-entity position="0 -5 0" ply-model="src: #castle" rotation="-90 0 0"></a-entity>
+      <a-entity position="-40 30 -70" ply-model="src: #cloud1" rotation="-90 -20 0"></a-entity>
+      <a-entity position="0 30 -60" ply-model="src: #cloud1" rotation="-90 0 0"></a-entity>
+      <a-entity position="40 30 -50" ply-model="src: #cloud1" rotation="-90 -10 0"></a-entity>
+      <a-sky color="#00BFFF" ></a-sky>
+      
+  </a-scene>
     </div>
   `
